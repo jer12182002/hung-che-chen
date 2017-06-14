474039c91e4fa8da227560529953f68856d55b99
jer12182002 <jer12182002@gmail.com>
2017-06-14 09:45:04 -0400

    commit5

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
 /*********************************************************************************
-* WEB322 – Assignment 02
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
-* of this assignment has been copied manually or electronically from any other source
-* (including 3rd party web sites) or distributed to other students. *
-* Name: __hung-che,chen____ Student ID: _115472169____ Date: __June,11,2017________ *
-* Online (Heroku) Link: _https://warm-taiga-21988.herokuapp.com________
-* ********************************************************************************/
+*  WEB322 –Assignment02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ___Xiaochen Wang__ Student ID: ___015297153_____ Date: ____09-06-2017__
+*
+*  Online (Heroku) Link:  https://cryptic-waters-33866.herokuapp.com
+*
+********************************************************************************/ 
 var express = require("express");
 var app = express();
 var path = require("path");
+var data_service = require("./data-service.js");
 
-var data_service = require('./data-service.js');
 var HTTP_PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-
-// call this function after the http server starts listening for requests
 function onHttpStart() {
-  console.log("Express http server listening on port:" + HTTP_PORT);
+    console.log("Express http server listening on: " + HTTP_PORT);
+    return new Promise (function(res,req){
+    data_service.initialize().then(function(data){
+      console.log(data)
+    }).catch(function(err){
+      console.log(err);
+    });
+});
 }
+
+// Load CSS file
+app.use(express.static('public'));
+// alternative method.
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
+   //res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
    res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-   res.sendFile(path.join(__dirname + "/views/about.html"));
-});
-
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
-
-app.get("/",(req,res)=>{
-   res.sendFile(path.join(__dirname + "/views/home.html"));
+  res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
 app.get("/employees", function(req,res){
@@ -96,21 +102,12 @@ app.use(function(req, res) {
 
 app.listen(HTTP_PORT, onHttpStart);
 
-
-/*
-app.get("/employees",(req,res)=>{
-  if(req.query.status){
-    res.json({message: req.query.status});
-  }else if(req.query.manager){
-    res.json({message: req.query.manager});
-  }else if(req.query.department){
-    res.json({message: req.query.department});
-  }else{
-    //res.status(404);
-    //res.send("Page Not Found");
-    res.status(404).send("Page Not Found");
-  }
-});
-
-app.listen("/data-service.js");
-*/
+//Alternative solution for app.listen method.
+// app.listen(HTTP_PORT, function(res,req){
+//   console.log("Express http server listening on: " + HTTP_PORT);
+//   data_service.initialize().then(function(data){
+//       console.log(data)
+//     }).catch(function(err){
+//       console.log(err);
+//     });
+// });
