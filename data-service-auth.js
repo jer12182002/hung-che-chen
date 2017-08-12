bd14f141e17c1c6ebf8159007bd52a5bfe3cd59b
jer12182002 <jer12182002@gmail.com>
2017-08-12 17:18:08 -0400

    Almost Done

--- data-service-auth.js
+++ data-service-auth.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const chalk = require('chalk');
 let Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
@@ -10,10 +11,16 @@ var userSchema = new Schema({
 });
 var Comment; // to be defined on new connection (see initialize)
 
-var dbURI = "mongodb://ucholoev:2423645Ular_@ds049486.mlab.com:49486/web322_a7"
+var dbURI = "mongodb://web322_18:hchen224@ds051903.mlab.com:51903/web322_a7";
 
 module.exports.initialize = () => {
-    console.log("step 1 initialize!")
+    console.log("============================================");
+    console.log("===                                      ===");
+    console.log("===    MongoDB for auth initialize       ===");
+    console.log("===                                      ===");
+    console.log("============================================");
+    console.log("\n")
+    console.log(">>> DB dbURI: " + dbURI + " <<<");
     console.log("\n")
     return new Promise((resolve, reject) => {
         let db = mongoose.createConnection(dbURI);
@@ -22,46 +29,101 @@ module.exports.initialize = () => {
         });
         db.once('open', () => {
             Comment = db.model("users", userSchema);
-            resolve("MongoDB good");
+            Comment.remove({ }, function (err) { });
+            resolve("Secess initialize MongoDB");
         });
     });
 };
 
 module.exports.registerUser = (userData) => {
-    console.log("register User!!")
+    console.log("============================================");
+    console.log("===                                      ===");
+    console.log("===           registerUser Function      ===");
+    console.log("===                                      ===");
+    console.log("============================================");
+    console.log("\n");
     return new Promise((resolve, reject) => {
         if (userData.password != userData.password2) {
             reject("Passwords do not match.");
         } else {
-        let newUser = new Comment(userData);
-        newUser.save((err) => {
-            resolve();
-        }).catch((err) => {
-            if (err) {
-                if (err.code == 11000) {
-                    reject("User Name already taken");
-                } else {
-                    reject("There was an error creating the user: ${user}");
+            let newUser = new Comment(userData);
+             bcrypt.genSalt(10, function(err, salt) { // Generate a "salt" using 10 rounds
+                if (err) {
+                    reject("There was an error encrypting the password");
                 }
-            }
-            
-        });
-    }});
-}
+                bcrypt.hash(userData.password, salt, function(err, hash) { // encrypt the password: "myPassword123"
+                    // TODO: Store the resulting "hash" value in the DB
+                    console.log(chalk.yellow(hash));
+                    newUser.password = hash;
+                    console.log(chalk.red(newUser));
+                    newUser.save((err) => {
+                        console.log(chalk.blue("===   Object is saving in the database.  ==="));
+                        console.log(chalk.blue("============================================"));
+                        console.log(userData);
+                        console.log(chalk.blue("============================================"));
+                        console.log(chalk.blue("This is User object id from userSchema: " + newUser._id));
+                        console.log(chalk.green(newUser));
+                        resolve();
+                    }).catch((err) => {
+                        if (err) {
+                            if (err.code == 11000) {
+                                reject("User Name already taken");
+                            } else {
+                                reject("There was an error creating the user: ${user}");
+                            }
+                        }
+            // reject("There was an error creating the user222222");
+                    });
+                });
+             });
+        }});
+    }
 
 module.exports.checkUser = (userData) =>{
-    console.log("check user !!!")
+    console.log(chalk.blue("============================================="));
+    console.log(chalk.blue("===                                       ==="));
+    console.log(chalk.blue("===     This is checkUser function        ==="));
+    console.log(chalk.blue("===                                       ==="));
+    console.log(chalk.blue("============================================="));
+    console.log(">>> userName: " + chalk.green(userData.user) + " <<<");
+    console.log(">>> userName: " + chalk.green(userData.password) + " <<<");
+
     return new Promise((resolve, reject) => {
         Comment.find({user: userData.user}).exec().then((user) => {
-        console.log(("Sucess!!!!!!" + user));
+        console.log(chalk.bgCyan("Sucess!!!!!!" + user));
         if (user == null) {
-            reject('Can not find user: ' + userData.user);
-        } else if (user[0].password != userData.password) {
-            reject('Incorrect Password: ' + user[0].user );
+            reject('Unable to find user: ' + userData.user);
+        } else {
+           hash = user[0].password;
+            console.log(chalk.yellow("++++++++++++++++++++++++++++++++++++++++++++" + user[0].password));
+
+            bcrypt.compare(userData.password, hash).then((res) => {
+                res === true; //if it matches and res === false if it does not match
+                console.log(chalk.bgCyan(hash));
+                resolve();
+            });
+
+            bcrypt.compare(userData.password, hash).then((res) => {
+                res === false; //if it matches and res === false if it does not match
+                console.log(chalk.red(">>>>>>>>>>>>>>>>>>>>"+ hash));
+                reject("Unable to find user: " + userData.user);
+            });
         }
-        resolve();
         }).catch((err) => {
-            reject("Can not find user: " + userData.user);
+            console.log(chalk.bgCyan("There is Error"));
+            reject("Unable to find user: " + userData.user);
+        });
+    });
+};
+
+module.exports.updatePassword = (userData) => {
+    return new Promise((resolve, reject) => {
+        Comment.update({ user: userData.user },
+        { $set: { password: hash } },
+        { multi: false }).exec().then((res) => {
+            resolve();
+        }).catch((err) => {
+            reject("There was an error updating the password for " + userData.user);
         });
     });
 };
